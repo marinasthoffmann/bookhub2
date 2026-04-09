b9d575f08b7fec6101120145a3162142cb62d249
Marina Hoffmann <marinasthoffmann@gmail.com>
2026-04-09 20:19:23 +0200

    chore: validate data

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -10,13 +10,25 @@ const livroSchema = new mongoose.Schema(
     autor: {
       type: mongoose.Schema.Types.ObjectId, 
       ref: 'autores', 
-      required: [ true, "O autor do livro é obrigatório." ]
+      required: [ true, "O autor do livro é obrigatório." ],      
     },
     editora: {
       type: String, 
-      required: [ true, "A editora do livro é obrigatória." ]
+      required: [ true, "A editora do livro é obrigatória." ],
+      enum: {
+        values: ["Casa do Código", "Alura"],
+        message: "A editora {VALUE} deve ser 'Casa do Código' ou 'Alura'."
+      }
     },
-    numeroPaginas: {type: Number}
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve ser entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
+    }
   }
 );
 
